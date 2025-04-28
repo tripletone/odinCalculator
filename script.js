12d55be0f6c1b0eb25875945f6e402131ba8d1a9
Kevin <151878377+tripletone@users.noreply.github.com>
2025-04-27 21:53:34 -0500

    added in operate function and changed variable names to avoid confusion

--- script.js
+++ script.js
@@ -14,8 +14,20 @@ const divide = (num1, num2) => {
     return num1 / num2;
 };
 
-let num1;
+let userNum1;
 
-let num2;
+let userNum2;
 
 let operator;
+
+const operate = (num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
