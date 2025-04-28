0c9f9ca8237733bc668e7f09d92e1a77730ad6f4
Kevin <151878377+tripletone@users.noreply.github.com>
2025-04-27 21:46:58 -0500

    added in basic operators

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+
